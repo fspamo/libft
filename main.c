ffcdf06ac08c80c128fd55df39ab31b00c18dfe6
fspamo <113922120+fspamo@users.noreply.github.com>
2026-01-13 23:49:41 +0300

    char, endl, str, its all the same...

--- main.c
+++ main.c
@@ -2,27 +2,8 @@
 #include <stdlib.h>
 #include "libft.h"
 
-void to_upper(unsigned int i, char *c)
-{
-	(void)i;
-	while(c[i])
-	{
-		if (*c >= 'a' && *c <= 'z')
-			*c -= 32;
-		printf("%c", c[i]);
-		i++;
-	}
-}
-
 int main(void)
 {
-
-	char str[] = "hello";
-	printf("%s \n", str);
-	ft_striteri(str, to_upper);
-
-	printf("Result: %s\n", str);
-	// RESULT SHOULD BE UPPERCASE HELLO WORLD
-	return (0);
+	char str[] = "flex memory area lol";
+	ft_putendl_fd(str, 1);
 }
-
