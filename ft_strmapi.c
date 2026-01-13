716b40529ceb983f32b1c8173be25b5b1877d079
Cahit Atilla Bozkurt <cbozkurt@k2m09s05.42kocaeli.com.tr>
2026-01-13 18:58:30 +0300

    strmapi is done, theres a room for improvement, indeed.

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,11 +6,25 @@
 /*   By: cbozkurt <cbozkurt@student.42kocaeli.com.  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 17:12:03 by cbozkurt          #+#    #+#             */
-/*   Updated: 2026/01/13 17:16:05 by cbozkurt         ###   ########.fr       */
+/*   Updated: 2026/01/13 18:51:44 by cbozkurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *ft_strmapi(char const *s, char (*f)(unsigned int, char));
-{
+#include "libft.h"
+#include <stdlib.h>
 
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	int		i;
+	char	*value;
+
+	i = 0;
+	value = malloc((ft_strlen(s) + 1) * sizeof(char));
+	while (i < ft_strlen(s))
+	{
+		value[i] = f(i, s[i]);
+		i++;
+	}
+	value[i] = '\0';
+	return (value);
 }
--- main.c
+++ main.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "libft.h"
+
+char    ft_test(unsigned int i, char c)
+{
+    (void)i;
+    if (c >= 'a' && c <= 'z')
+        return (c - 32); // to uppercase
+    return (c);
+}
 
 int main(void)
 {
-	printf("%s\n", ft_itoa(-321897));
+    char *result;
+
+    result = ft_strmapi("cahit", ft_test);
+    if (!result)
+        return (1);
+
+    printf("Result: %s\n", result);
+	// RESULT SHOULD BE UPPERCASE HELLO WORLD
+    free(result);
+    return (0);
 }
+
