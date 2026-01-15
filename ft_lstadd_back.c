1c261bbcf1f45d56e5c9dc20b76fe354e2a868fd
fspamo <113922120+fspamo@users.noreply.github.com>
2026-01-15 18:01:53 +0300

    linked lists understandingen

---
+++ ft_lstadd_back.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbozkurt <cbozkurt@student.42kocaeli.com.  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/15 15:54:34 by cbozkurt          #+#    #+#             */
+/*   Updated: 2026/01/15 17:02:58 by cbozkurt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*temp;	
+
+	temp = *lst;
+
+	while(temp->next)
+		temp = temp->next;
+	
+	temp->next = new;
+}
--- main.c
+++ main.c
@@ -6,15 +6,9 @@ int main(void)
 {
 	char *name = "cahit";
 	char *isim = "sadik";
+	// char *nombre = "linus";
 
-	t_list *node = ft_lstnew(name);
-	t_list *node0 = ft_lstnew(isim);
 
-	ft_lstadd_front(&node0, node);
 
-	printf("%s", (char *)node->content);
-	printf("%s", (char *)node->next->content);
-
-	printf("%d", ft_lstsize(node));
+	return (0);
 }
-
