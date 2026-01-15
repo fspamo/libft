9e533b2d6cd7e4f3783940c830c2d1d7d0b120da
fspamo <113922120+fspamo@users.noreply.github.com>
2026-01-15 21:13:15 +0300

    study more

--- main.c
+++ main.c
@@ -6,21 +6,34 @@
 /*   By: cbozkurt <cbozkurt@student.42kocaeli.com.  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 18:41:49 by cbozkurt          #+#    #+#             */
-/*   Updated: 2026/01/15 18:48:57 by cbozkurt         ###   ########.fr       */
+/*   Updated: 2026/01/15 20:41:05 by cbozkurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include <stdlib.h>
-#include "libft.h"
+#include "libft.h" // assuming t_list is defined here
+
+void print_content(void *content)
+{
+    printf("%s\n", (char *)content);
+}
 
 int main(void)
 {
-	char *name = "cahit";
-	char *isim = "sadik";
-	// char *nombre = "linus";
+	t_list *node;
 
 
+	*node = ft_lstnew(node);
+    t_list *list = ft_lstnew("Hello");
+    ft_lstadd_back(&list, ft_lstnew("World"));
+    ft_lstadd_back(&list, ft_lstnew("42"));
 
-	return (0);
+    ft_lstiter(list, print_content);
+
+    // Output:
+    // Hello
+    // World
+    // 42
+    return 0;
 }
+
