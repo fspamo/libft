045051e76cd9d69a61d32ff0ba09be1a0a977920
fspamo <xahit1337@gmail.com>
2026-01-17 13:49:07 +0300

    finished

---
+++ ft_lstmap.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbozkurt <cbozkurt@student.42kocaeli.com.  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/17 13:18:19 by cbozkurt          #+#    #+#             */
+/*   Updated: 2026/01/17 13:42:16 by cbozkurt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list *n_list;
+	t_list *temp;
+
+	if(!lst || !f || !del)
+		return (NULL);
+
+	n_list = NULL;
+	while(lst)
+	{
+		temp = ft_lstnew(f(lst->content));
+        if (!temp)
+        {
+            ft_lstclear(&n_list, del);
+            return (NULL);
+        }
+        ft_lstadd_back(&n_list, temp);
+        lst = lst->next;
+	}
+	return (n_list);
+}
