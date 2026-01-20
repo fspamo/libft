1ac6dfc722744d17bbce7ee5981fa0cacb8fe341
Cahit Atilla Bozkurt <cbozkurt@k1m07s01.42kocaeli.com.tr>
2026-01-20 14:51:52 +0300

    lstadd_front re-written

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: cbozkurt <cbozkurt@student.42kocaeli.com.  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/14 17:30:42 by cbozkurt          #+#    #+#             */
-/*   Updated: 2026/01/18 14:26:33 by cbozkurt         ###   ########.fr       */
+/*   Created: 2026/01/20 13:06:57 by cbozkurt          #+#    #+#             */
+/*   Updated: 2026/01/20 13:42:09 by cbozkurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
+	if(!new || !lst)
+		return ;
 	new->next = *lst;
 	*lst = new;
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: cbozkurt <cbozkurt@student.42kocaeli.com.  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 18:41:49 by cbozkurt          #+#    #+#             */
-/*   Updated: 2026/01/19 16:45:27 by cbozkurt         ###   ########.fr       */
+/*   Updated: 2026/01/20 13:46:04 by cbozkurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,22 @@ int main(void)
 	char *str = "merhaba";
 	// char *next = "ben";
 
+	t_list *lstnext = NULL;
+
 	t_list *lst = ft_lstnew((void *)str);
 	// t_list *lstnext = ft_lstnew((void *)next);
+
+	printf("%s \n", (char *)lst->content);
+	printf("%p \n", (char *)lst->next);
+	printf("%d \n", ft_lstsize(lst));
+
+	ft_lstadd_front(&lst, lstnext);
 
 	// lst->next = lstnext;
 	// lstnext->next = NULL;
 	printf("%s \n", (char *)lst->content);
-	// printf("%s \n", (char *)lst->next->next);
+	printf("%s \n", (char *)lst->next->content);
+	printf("%p \n", (char *)lst->next->next);
 	printf("%d \n", ft_lstsize(lst));
+	
 }
