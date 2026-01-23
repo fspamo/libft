c36678f6a426bec10a560e42b339d997223220fa
fspamo <xahit1337@gmail.com>
2026-01-23 10:40:56 +0300

    lstiter re-written not tested

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -5,18 +5,18 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: cbozkurt <cbozkurt@student.42kocaeli.com.  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/15 19:30:07 by cbozkurt          #+#    #+#             */
-/*   Updated: 2026/01/18 16:57:47 by cbozkurt         ###   ########.fr       */
+/*   Created: 2026/01/23 09:54:05 by cbozkurt          #+#    #+#             */
+/*   Updated: 2026/01/23 10:07:45 by cbozkurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstiter(t_list *lst, void (*f)(void *))
+void ft_lstiter(t_list *lst, void (*f)(void *))
 {
-	if (!lst || !f)
+	if (!f || !lst)
 		return ;
-	while (lst)
+	while(lst)
 	{
 		f(lst->content);
 		lst = lst->next;
