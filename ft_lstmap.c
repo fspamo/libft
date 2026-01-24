d2bcb87a1dbddc52cd386f4b768000ed7aa6aca7
fspamo <cahitatillab@gmail.com>
2026-01-24 04:01:04 +0300

    lstmap is either fraustrating or i dont understand crap about C

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -5,31 +5,31 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: cbozkurt <cbozkurt@student.42kocaeli.com.  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/17 13:18:19 by cbozkurt          #+#    #+#             */
-/*   Updated: 2026/01/18 15:55:02 by cbozkurt         ###   ########.fr       */
+/*   Created: 2026/01/24 00:46:54 by cbozkurt          #+#    #+#             */
+/*   Updated: 2026/01/24 03:58:48 by cbozkurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*n_list;
+	t_list	*new_list;
 	t_list	*temp;
+	void	*new_content;
+	void	*to_del;
 
-	if (!lst || !f || !del)
+	new_list = malloc(sizeof(t_list));
+	temp = lst;
+	if(!lst || !f || !del || !new_list)
 		return (NULL);
-	n_list = NULL;
-	while (lst)
+	while(temp)
 	{
-		temp = ft_lstnew(f(lst->content));
-		if (!temp)
-		{
-			ft_lstclear(&n_list, del);
-			return (NULL);
-		}
-		ft_lstadd_back(&n_list, temp);
-		lst = lst->next;
+		new_content = f(temp->content);
+		if(!new_content)
+			del(temp->content);
+		else
+			new_list->content = new_content;
+		temp = temp->next;
+		new_list = new_list->next;
 	}
-	return (n_list);
+	new_list->next = NULL;	
 }
