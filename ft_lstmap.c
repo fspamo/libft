72afcf9e16b14337654143564613586a83d94c46
salmonella <cahitatillab@gmail.com>
2026-01-25 13:48:43 +0300

    lstmap is in progress

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -5,31 +5,35 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: cbozkurt <cbozkurt@student.42kocaeli.com.  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/24 00:46:54 by cbozkurt          #+#    #+#             */
-/*   Updated: 2026/01/24 03:58:48 by cbozkurt         ###   ########.fr       */
+/*   Created: 2026/01/24 23:55:08 by cbozkurt          #+#    #+#             */
+/*   Updated: 2026/01/25 00:57:24 by cbozkurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*new_list;
+	t_list	*new_lst;
 	t_list	*temp;
-	void	*new_content;
-	void	*to_del;
+	void	*current_content;
 
-	new_list = malloc(sizeof(t_list));
 	temp = lst;
-	if(!lst || !f || !del || !new_list)
+	if (!lst || !f || !del)
 		return (NULL);
-	while(temp)
+	while (temp)
 	{
-		new_content = f(temp->content);
-		if(!new_content)
-			del(temp->content);
+		current_content = temp->content;
+		f(current_content);
+		if (!current_content)
+			return (NULL);
 		else
-			new_list->content = new_content;
+		{
+			new_list = malloc(sizeof(t_list));
+			new_lst->content = current_content; 
+		}
 		temp = temp->next;
-		new_list = new_list->next;
+		new_lst = new_lst->next;
 	}
-	new_list->next = NULL;	
+	return (new_lst);
 }
