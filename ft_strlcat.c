343534511b86950300146514d4de5d77d8c438a8
Cahit Atilla Bozkurt <cbozkurt@k2m08s01.42kocaeli.com.tr>
2026-01-27 14:36:32 +0300

    strlcat re-written

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: cbozkurt <cbozkurt@student.42kocaeli.com.  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/08 09:28:06 by cbozkurt          #+#    #+#             */
-/*   Updated: 2026/01/26 18:17:26 by cbozkurt         ###   ########.fr       */
+/*   Updated: 2026/01/27 14:34:39 by cbozkurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,20 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	int		i;
-	int		p_append;
-	char	*real_dst;
-	int		dst_len;
-	int		src_len;
+	size_t	i;
+	size_t	dst_len;
+	size_t	src_len;
 
-	src_len = ft_strlen(src);
-	dst_len = ft_strlen(dst);
-	real_dst = dst;
 	i = 0;
-	p_append = size - dst_len - 1;
-	if(size <= (size_t)dst_len)
-		return (size + (size_t)src_len);
-	while (p_append > 0)
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	while (src[i] && dst_len + i + 1 < size)
 	{
 		dst[dst_len + i] = src[i];
-		p_append--;
+		i++;
 	}
-	return (ft_strlen(real_dst) + src_len);
+	if (size <= dst_len)
+		return (size + src_len);
+	dst[dst_len + i] = '\0';
+	return (dst_len + src_len);
 }
-//
-// #include <stdio.h>
-//
-// int main(void)
-// {
-// 	char dest[30]; memset(dest, 0, 30);                                   
-// 	char * src = (char *)"AAAAAAAAA";
-// 	dest[0] = 'B';
-//
-// 	printf("%d", (int)ft_strlcat(dest, src, 6));
-// }
