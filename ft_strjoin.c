f807cc7607783ce12653368014b9faaa6de29ffb
Cahit Atilla Bozkurt <cbozkurt@k2m08s01.42kocaeli.com.tr>
2026-01-28 01:39:15 +0300

    MOK

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: cbozkurt <cbozkurt@student.42kocaeli.com.  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/11 12:22:24 by cbozkurt          #+#    #+#             */
-/*   Updated: 2026/01/21 13:38:15 by cbozkurt         ###   ########.fr       */
+/*   Updated: 2026/01/28 01:38:14 by cbozkurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	i = 0;
 	j = 0;
 	str_sum = ft_strlen(s1) + ft_strlen(s2);
-	str = malloc(str_sum);
+	str = malloc(str_sum + 1);
 	if (!str)
 		return (NULL);
 	while (i < ft_strlen(s1))
